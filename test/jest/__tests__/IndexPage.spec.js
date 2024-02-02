161b0132f79b7642a8fd0f463f0717942b2fb0a3
= <=>
2024-02-02 11:07:50 -0800

    initial commit

---
+++ test/jest/__tests__/IndexPage.spec.js
@@ -0,0 +1,25 @@
+// IndexPage.spec.js
+import IndexPage from 'src/pages/IndexPage.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe('IndexPage', () => {
+  it('should render correct contents', () => {
+    const wrapper = shallowMount(IndexPage)
+    let header = wrapper.find('.htmlClass h1')
+    expect(header.exists()).toBe(true)
+    expect(header.text())
+      .toBe('Vue is awesome.')
+  })
+
+})
+it('check text content to be as defined in variable', () => {
+  const wrapper = shallowMount(IndexPage, {
+    data () {
+      return {
+        title: 'Vue is awesome.'
+      }
+    }
+  })
+  let header = wrapper.find('.htmlClass h1')
+  expect(header.text()).toBe('Vue is awesome.')
+})
